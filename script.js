c951786099b916c6d39781da75a7c7eb8353cf46
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-04 06:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Your code here.
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Mouse Down Event
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+// Mouse Leave Event
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+// Mouse Up Event
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+// Mouse Move Event
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return; // Stop the function from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Adjust scroll speed
+  slider.scrollLeft = scrollLeft - walk;
+});
